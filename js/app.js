360bc4df4c97974290ef85397360cbb236df2f93
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-09-20 22:48:57 +0200

    add slider for gallery

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@ colorList
     .eq(0).css('background-color' , '#FEBB48').end()
     .eq(1).css('background-color' , '#36487a').end()
     .eq(2).css('background-color' , '#001a61').end()
-    .eq(3).css('background-color' , '#f10b31');
+    .eq(3).css('background-color' , '#f10b31').end()
+    .eq(4).css('background-color' , '#212529');
 
 colorList.click(function(){
     $('link[href*="theme"]').attr('href' , $(this).attr('data-value'));
